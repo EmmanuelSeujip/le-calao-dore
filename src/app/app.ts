2729247e1e3564d9c3ec1c7c8c1adae657678d84
EmmanuelSeujip <emmanuelseujip@gmail.com>
2026-06-22 12:43:40 +0100

    feat(hero): section hero avec image de fond dynamique

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeader } from "./app-header/app-header";
+import { Hero } from "./hero/hero";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppHeader],
+  imports: [RouterOutlet, AppHeader, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
